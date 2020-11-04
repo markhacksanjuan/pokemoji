df49a92d67ff424aad56619de9b7228d9849e9a8
Mark <mark.sanjuan.sole@gmail.com>
2020-11-04 10:24:18 +0100

    working on it

--- js/emojisObject.js
+++ js/emojisObject.js
@@ -8,238 +8,238 @@ const emojiArr = [
     },
     {
     name: 'emoji-frustated',
-    src: './images/emojisAlone/emoji-frustated.png',
+    src: '../images/emojisAlone/emoji-frustated.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-frustated-2',
-    src: './images/emojisAlone/emoji-frustated-2.png',
+    src: '../images/emojisAlone/emoji-frustated-2.png',
     attack: 5,
     defense: 200,
     alive: true   
     },
     {
     name: 'emoji-happy-1',
-    src: './images/emojisAlone/emoji-happy-1.png',
+    src: '../images/emojisAlone/emoji-happy-1.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-happy-2',
-    src: './images/emojisAlone/emoji-happy-2.png',
+    src: '../images/emojisAlone/emoji-happy-2.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-happy-3',
-    src: './images/emojisAlone/emoji-happy-3.png',
+    src: '../images/emojisAlone/emoji-happy-3.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-happy-4',
-    src: './images/emojisAlone/emoji-happy-4.png',
+    src: '../images/emojisAlone/emoji-happy-4.png',
     attack: 5,
     defense: 0,
     alive: true   
     },
     {
     name: 'emoji-happy-5',
-    src: './images/emojisAlone/emoji-happy-5.png',
+    src: '../images/emojisAlone/emoji-happy-5.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-hug',
-    src: './images/emojisAlone/emoji-hug.png',
+    src: '../images/emojisAlone/emoji-hug.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-kiss-1',
-    src: './images/emojisAlone/emoji-kiss-1.png',
+    src: '../images/emojisAlone/emoji-kiss-1.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-kiss-2',
-    src: './images/emojisAlone/emoji-kiss-2.png',
+    src: '../images/emojisAlone/emoji-kiss-2.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-kiss-3',
-    src: './images/emojisAlone/emoji-kiss-3.png',
+    src: '../images/emojisAlone/emoji-kiss-3.png',
     attack: 5,
     defense: 0,
     alive: true   
     },
     {
     name: 'emoji-laugh-1',
-    src: './images/emojisAlone/emoji-laugh-1.png',
+    src: '../images/emojisAlone/emoji-laugh-1.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-laugh-2',
-    src: './images/emojisAlone/emoji-laugh-2.png',
+    src: '../images/emojisAlone/emoji-laugh-2.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-love',
-    src: './images/emojisAlone/emoji-love.png',
+    src: '../images/emojisAlone/emoji-love.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-mouth-open',
-    src: './images/emojisAlone/emoji-mouth-open.png',
+    src: '../images/emojisAlone/emoji-mouth-open.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-mute',
-    src: './images/emojisAlone/emoji-mute.png',
+    src: '../images/emojisAlone/emoji-mute.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-none',
-    src: './images/emojisAlone/emoji-none.png',
+    src: '../images/emojisAlone/emoji-none.png',
     attack: 5,
     defense: 0,
     alive: true   
     },
     {
     name: 'emoji-none-2',
-    src: './images/emojisAlone/emoji-none-2.png',
+    src: '../images/emojisAlone/emoji-none-2.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-nope',
-    src: './images/emojisAlone/emoji-nope.png',
+    src: '../images/emojisAlone/emoji-nope.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-no-way',
-    src: './images/emojisAlone/emoji-no-way.png',
+    src: '../images/emojisAlone/emoji-no-way.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-sad-1',
-    src: './images/emojisAlone/emoji-sad-1.png',
+    src: '../images/emojisAlone/emoji-sad-1.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-sad-2',
-    src: './images/emojisAlone/emoji-sad-2.png',
+    src: '../images/emojisAlone/emoji-sad-2.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-saint',
-    src: './images/emojisAlone/emoji-saint.png',
+    src: '../images/emojisAlone/emoji-saint.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-shut',
-    src: './images/emojisAlone/emoji-shut.png',
+    src: '../images/emojisAlone/emoji-shut.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-sleep',
-    src: './images/emojisAlone/emoji-sleep.png',
+    src: '../images/emojisAlone/emoji-sleep.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-smile-1',
-    src: './images/emojisAlone/emoji-smile-1.png',
+    src: '../images/emojisAlone/emoji-smile-1.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-smile-2',
-    src: './images/emojisAlone/emoji-smile-2.png',
+    src: '../images/emojisAlone/emoji-smile-2.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-smile-3',
-    src: './images/emojisAlone/emoji-smile-3.png',
+    src: '../images/emojisAlone/emoji-smile-3.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-smile-4',
-    src: './images/emojisAlone/emoji-smile-4.png',
+    src: '../images/emojisAlone/emoji-smile-4.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-smile-5',
-    src: './images/emojisAlone/emoji-smile-5.png',
+    src: '../images/emojisAlone/emoji-smile-5.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-tongue-1',
-    src: './images/emojisAlone/emoji-tongue-1.png',
+    src: '../images/emojisAlone/emoji-tongue-1.png',
     attack: 5,
     defense: 0,
     alive: true   
     },
     {
     name: 'emoji-tongue-2',
-    src: './images/emojisAlone/emoji-tongue-2.png',
+    src: '../images/emojisAlone/emoji-tongue-2.png',
     attack: 5,
     defense: 100,
     alive: true   
     },
     {
     name: 'emoji-tongue-3',
-    src: './images/emojisAlone/emoji-tongue-3.png',
+    src: '../images/emojisAlone/emoji-tongue-3.png',
     attack: 5,
     defense: 0,
     alive: true   
     },
     {
     name: 'emoji-wink',
-    src: './images/emojisAlone/emoji-wink.png',
+    src: '../images/emojisAlone/emoji-wink.png',
     attack: 5,
     defense: 100,
     alive: true   
@@ -251,55 +251,55 @@ const emojiArr = [
 const emojiWeaponArr = [
     {
     name: 'emoji-dislike',
-    src: './images/emojisAlone/emoji-dislike.png',
+    src: '../images/emojisAlone/emoji-dislike.png',
     attack: 20,
     defense: 0
     },
     {
     name: 'emoji-hand',
-    src: './images/emojisAlone/emoji-hand.png',
+    src: '../images/emojisAlone/emoji-hand.png',
     attack: 20,
     defense: 0
     },
     {
     name: 'emoji-like',
-    src: './images/emojisAlone/emoji-like.png',
+    src: '../images/emojisAlone/emoji-like.png',
     attack: 25,
     defense: 0
     },
     {
     name: 'emoji-love-arrow',
-    src: './images/emojisAlone/emoji-love-arrow.png',
+    src: '../images/emojisAlone/emoji-love-arrow.png',
     attack: 30,
     defense: 0
     },
     {
     name: 'emoji-love-heart',
-    src: './images/emojisAlone/emoji-love-heart.png',
+    src: '../images/emojisAlone/emoji-love-heart.png',
     attack: 10,
     defense: 0
     },
     {
     name: 'emoji-love-pink',
-    src: './images/emojisAlone/emoji-love-pink.png',
+    src: '../images/emojisAlone/emoji-love-pink.png',
     attack: 15,
     defense: 0
     },
     {
     name: 'emoji-love-two',
-    src: './images/emojisAlone/emoji-love-two.png',
+    src: '../images/emojisAlone/emoji-love-two.png',
     attack: 20,
     defense: 0
     },
     {
     name: 'emoji-punch',
-    src: './images/emojisAlone/emoji-punch.png',
+    src: '../images/emojisAlone/emoji-punch.png',
     attack: 30,
     defense: 0
     },
     {
     name: 'emoji-victory',
-    src: './images/emojisAlone/emoji-victory.png',
+    src: '../images/emojisAlone/emoji-victory.png',
     attack: 5,
     defense: 0
     },
